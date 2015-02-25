af3c273e62d4c64704f48f44c61e1c5d75d48f5e
arafeek <adam_rafeek@hotmail.com>
2015-02-25 15:36:06 -0500

    updates

--- slider.js
+++ slider.js
@@ -1,3 +1,6 @@
+// Updated 24-02-15
+// Adam Rafeek
+
 $(document).ready(function(){
 
 	var currentPosition = 0;
@@ -9,6 +12,10 @@ $(document).ready(function(){
 	var autoSlideEnabled = autoSlide;
 	var carouselTimer = null;
 	var slideshowControls = $('#slideshow-controls');
+    var slidesContainer = $('#slidesContainer');
+    // Touch vars
+	var startX;
+	var startTime;
 
 	init();
 
@@ -28,10 +35,11 @@ $(document).ready(function(){
 	    resetTimer();
   	});
 
-  	// $('#slidesContainer').hover(
-  	// 	function() { if (autoSlide && autoSlideEnabled) clearInterval(carouselTimer);}, // on mouseover
-  	// 	function() { if (autoSlide && autoSlideEnabled) resetTimer(); } // on mouseleave
-  	// );
+    // Clicking on the slideshow moves the slide from left to right
+  	$('#slidesContainer').bind('click', function() {
+  		animateSlider(currentPosition+1);
+  		resetTimer();
+  	});
 
 	// removed :#slidesContainer for now since it caused an issue with the play/plause button displaying correctly
   	$('.animation-control-button').bind('click', function() {
@@ -82,10 +90,45 @@ $(document).ready(function(){
 	$('#slideshow-controls>img.button-pause').bind('click', function() {
 		$(this).css(({'display':'none'}));
 		$('#slideshow-controls>img.button-play').css(({'display':'inline'}))
-	})
-
+	});
 
 // HELPERS
+
+	function bindTouchHandlers() {
+		slidesContainer.on('touchstart', function(e){
+			var touchObj = e.originalEvent.changedTouches[0];
+			startX = touchObj.pageX;
+			startTime = new Date().getTime();
+			e.preventDefault();
+		});
+
+		slidesContainer.bind('touchmove',function(e){
+			e.preventDefault(); // prevent scrolling within the slideshow
+		});
+
+		slidesContainer.bind('touchend', function(e){
+			var endTime = new Date().getTime();
+			var elapsed = endTime - startTime;
+			var touchObj = e.originalEvent.changedTouches[0];
+			var dist = touchObj.pageX - startX;
+			// Min amt of pixels to register as a swipe
+			var minSwipeDist = 25;
+			console.log(elapsed);
+
+			if(Math.abs(dist) > minSwipeDist) {
+				if(dist > 0) {
+					animateSlider(currentPosition-1);
+					resetTimer();
+				} else if(dist < 0) {
+					animateSlider(currentPosition+1);
+					resetTimer();
+				}
+			} else if (elapsed < 250) {
+				// We'll count this as a click
+				animateSlider(currentPosition+1);
+			}
+		});
+	}
 
 	function animateSlider(newPosn) {
 		//manage currentPosition going out of bounds
@@ -133,6 +176,8 @@ $(document).ready(function(){
 			$('#slideshow-controls>img.button-play').css({'display':'none'});
 			$('#slideshow-controls>img.button-pause').css({'display':'none'});
 		}
+		// Enable touch event handeling
+		bindTouchHandlers();
 	}
 
 	function resetTimer() {
